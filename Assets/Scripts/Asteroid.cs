0770bc688e02c8d03445b1d829b85a442a6252b9
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2019-11-12 17:56:45 +0100

    Added Asteroid class
    Added asteroid class and updated AsteroidManager to hold a list of asteroids.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public int seedValue;
+    public Vector2 position;
+    public Asteroid(int initSeed)
+    {
+        this.seedValue = initSeed;
+    }
+}
--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -6,13 +6,21 @@ public class AsteroidManager : MonoBehaviour
 {
     public int numberOfAsteroids;
     public int[] AsteroidSeeds;
+    public List<Asteroid> asteroids;
 
     private void Start()
     {
+        asteroids = new List<Asteroid>();
         AsteroidSeeds = new int[numberOfAsteroids];
         for(int i=0;i<numberOfAsteroids;i++)
-        {
+        {//Later add in a check here to make sure that the asteroid seeds are never repeated
             AsteroidSeeds[i] = Random.Range(0, 1000000);
+            asteroids.Add(new Asteroid(AsteroidSeeds[i]));
         }
+    }
+
+    void AddNewAsteroid()
+    {
+
     }
 }
