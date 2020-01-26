ed1cfd58afc2e0afb62b1420c3e90c5b432ee577
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2020-01-26 16:29:01 +0100

    Missile Controller and Resource
    Updated MissileController with the values and methods necessary for missiles to have basic functionality.
    
    Added Resource Class

--- Assets/Scripts/MissileController.cs
+++ Assets/Scripts/MissileController.cs
@@ -5,14 +5,119 @@ using UnityEngine;
 public class MissileController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private int missileHealth;
+    private float missileBlastDamage;
+    private float missileBlastRadius;
+    private float speed;
+
+    public enum State
     {
-        
+        idle,
+        attacking,
+        launching,
+    }
+    private State currentState;
+
+    private Transform planetTransform;
+    private void Awake()
+    {
+        planetTransform = GameObject.Find("Planet(Clone)").transform; 
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        if (planetTransform == null)
+        {
+            planetTransform = GameObject.Find("Planet(Clone)").transform;
+        }
+        if (planetTransform == null)
+        {
+            Debug.Log("Error! missile cannot exist without planet!");
+            return;
+        }
+        if(transform.parent != planetTransform)
+        {
+            transform.SetParent(planetTransform);//Set the planet transform to be the parent of the missile
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(currentState == State.attacking)
+        {
+            Attacking();
+            return;
+        }
+        if(currentState == State.launching)
+        {
+            Launching();
+            return;
+        }
+        IdleAction();
+    }
+
+    //Missile behaviour methods:
+    public void SetState(State state)
+    {
+        currentState = state;
+    }
+    public State CurrentState()
+    {
+        return currentState;
+    }
+
+    private void Attacking()
+    {
+        //Point towards asteroid centre
+        //Move forwards speed*Time.deltaTime
+    }
+    private void Launching()
+    {
+        //Point away from asteroid centre
+        //Move forwards speed*Time.deltaTime
+        //Check if left planet radius. If so add to missile tracker and destroy game object
+    }
+    private void IdleAction()
+    {
+        //Do nothing
+    }
+
+    public void OnSelfDestruct()
+    {
+        //A method by which a player can self-destruct a missile.
+    }
+
+    //Missile basic methods:
+    public int GetHealth()
+    {
+        return missileHealth;
+    }
+    public float GetDamage()
+    {
+        return missileBlastDamage;
+    }
+    public float GetBlastRadius()
+    {
+        return missileBlastRadius;
+    }
+    public float GetSpeed()
+    {
+        return speed;
+    }
+    public void SetHealth(int health)
+    {
+        missileHealth = health;
+    }
+    public void SetDamage(float damage)
+    {
+        missileBlastDamage = damage;
+    }
+    public void SetRadius(float rad)
+    {
+        missileBlastRadius = rad;
+    }
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
     }
 }
---
+++ Assets/Scripts/Resources/Resource.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource
+{
+    //Basic values for all resources
+    private int value; //Everything has a value per tonne
+    private float probabilityOnAsteroid; //The likelihood that it will be on an asteroid at all
+    private float scarcity; //The average amount per asteroid if it is present
+
+    public int GetValue()
+    {
+        return value;
+    }
+    public void SetValue(int newValue)
+    {
+        value = newValue;
+    }
+    public float Probability()
+    {
+        return probabilityOnAsteroid;
+    }
+    public float Scarcity()
+    {
+        return scarcity;
+    }
+}
