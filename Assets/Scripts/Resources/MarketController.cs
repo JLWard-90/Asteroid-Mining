6d1577fd7918ee2247ebdb8727f54e2885e6915d
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2020-01-26 16:46:10 +0100

    Added MarketController
    Updated Resource Class with constructor.
    
    Added MarketController class with the following:
    resources list of type Resource
    AddResource() method
    GetValue() method

---
+++ Assets/Scripts/Resources/MarketController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketController : MonoBehaviour
+{
+    private List<Resource> resources;
+    
+    public void AddResource(string name, int value, float probability, float scarcity)
+    {
+        Resource newResource = new Resource(name, value, probability, scarcity);
+        resources.Add(newResource);
+    }
+    public int GetValue(string name)
+    {
+        Resource current;
+        for (int i=0; i<resources.Count; i++)
+        {
+            current = resources[i];
+            if (current.Name() == name)
+            {
+                return current.GetValue();
+            }
+        }
+        Debug.Log("Resource Not Found!");
+        return -1;
+    }
+}
--- Assets/Scripts/Resources/Resource.cs
+++ Assets/Scripts/Resources/Resource.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class Resource
 {
     //Basic values for all resources
+    private string resourceName; //name of resource
     private int value; //Everything has a value per tonne
     private float probabilityOnAsteroid; //The likelihood that it will be on an asteroid at all
     private float scarcity; //The average amount per asteroid if it is present
 
+    public Resource(string name, int value, float prob, float scarcity)
+    {
+        this.resourceName = name;
+        this.value = value;
+        this.probabilityOnAsteroid = prob;
+        this.scarcity = scarcity;
+    }
+
     public int GetValue()
     {
         return value;
+    }
+    public string Name()
+    {
+        return resourceName;
     }
     public void SetValue(int newValue)
     {
