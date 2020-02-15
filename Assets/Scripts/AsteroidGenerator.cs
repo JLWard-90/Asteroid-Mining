f2619811cf069aea59fb86a1858554b05e7f679b
JLWard-90 <53184853+JLWard-90@users.noreply.github.com>
2020-02-15 09:02:18 +0100

    AsteroidGenerator update
    Added GenerateAsteroid(int asteroidIndex) method to AsteroidGenerator. This method generates planet objects in the same way that the LevelController script does.

--- Assets/Scripts/AsteroidGenerator.cs
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class AsteroidGenerator : MonoBehaviour
 {
-    
+    AsteroidManager asteroidManager;
+    GameController gameController;
+    [SerializeField]
+    GameObject planetPrefab;
+    private void Awake()
+    {
+        asteroidManager = this.gameObject.GetComponent<AsteroidManager>();
+        gameController = this.gameObject.GetComponent<GameController>();
+    }
+    public void GenerateAsteroid(int asteroidIndex)
+    {
+        bool asteroidPreviouslyGenerated = asteroidManager.asteroids[asteroidIndex].prevgenerated;
+        if (asteroidPreviouslyGenerated)
+        {
+            return;
+        }
+        int seed = asteroidManager.asteroids[asteroidIndex].seedValue;
+        GameObject newPlanet = Instantiate(planetPrefab);
+        newPlanet.transform.position = new Vector3(asteroidManager.asteroids[asteroidIndex].position.x, asteroidManager.asteroids[asteroidIndex].position.y, 0);
+        Random.InitState(seed);
+        ShapeSettings.NoiseLayer[] noiseLayers = newPlanet.GetComponent<Planet>().shapeSettings.noiseLayers;
+        foreach (ShapeSettings.NoiseLayer noiseLayer in noiseLayers)
+        {
+            noiseLayer.noiseSettings.simpleNoiseSettings.centre = new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+            noiseLayer.noiseSettings.rigidNoiseSettings.centre = new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+        }
+        newPlanet.transform.SetParent(gameController.transform);
+        asteroidManager.planets[asteroidIndex] = newPlanet;
+    }
 }
